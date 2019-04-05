4148f394a97cdd699c11b52298f927948240d38e
jhsware <sebastian@urbantalk.se>
2019-04-05 22:02:14 +0200

    Simplify rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,56 +1,33 @@
 import babel from 'rollup-plugin-babel';
 import minify from 'rollup-plugin-babel-minify';
 import replace from 'rollup-plugin-replace';
-import nodeResolve from 'rollup-plugin-node-resolve';
 
-const baseConfig = (outputFormat) => {
-  const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 
-  let file;
-  switch (outputFormat) {
-    case 'umd':
-    case 'cjs':
-      file = 'dist/' + outputFormat + '/index' + (isProduction ? '.min' : '') + '.js';
-      break;
-
-    default:
-      throw new Error('Unsupported output format: ' + outputFormat);
-  }
-
-  return {
-    input: 'src/index.js',
-    plugins: [
-      nodeResolve(),
-      babel({
-        plugins: [
-          // Ensure "external-helpers" is only included in rollup builds
-          // Issue: https://github.com/rollup/rollup/issues/1595
-          '@babel/external-helpers'
-        ],
-        runtimeHelpers: true
-      }),
-      replace({
-        'process.env.NODE_ENV': JSON.stringify('production')
-      }),
-      isProduction ? minify({
-        comments: false,
-      }) : false,
-    ],
-    external: ['inferno', 'inferno-create-element'],
-    output: {
-      name: 'InfernoAnimation',
-      file: file,
-      format: outputFormat,
-      sourcemap: true,
-      globals: {
-        inferno: 'Inferno',
-        'inferno-create-element': 'infernoCreateElement'
-      },
-    },
-  };
-};
-
-export default [
-  baseConfig('cjs'),
-  baseConfig('umd'),
-];
+export default {
+  input: 'src/index.js',
+  output: {
+    file: `dist/index.cjs${isProduction ? '.min' : ''}.js`,
+    format: 'cjs',
+    sourcemap: true,
+    globals: {
+      inferno: 'Inferno',
+      'inferno-create-element': 'infernoCreateElement'
+    }
+  },
+  plugins: [
+    babel({
+      runtimeHelpers: true
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    isProduction ? minify({
+      comments: false,
+    }) : false
+  ],
+  external: [
+    'inferno',
+    'inferno', 'inferno-create-element'
+  ]
+}
